b9ea5c61373c6de7619db204cd54206519109826
VienNguyen-Dev <149781283+VienNguyen-Dev@users.noreply.github.com>
2023-11-12 10:41:11 +0700

    improving dark mode and refactoring app compoinent

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 
 import './App.css';
+// import About from './compoinents/About';
 import Navbar from './compoinents/Navbar';
 import TextForm from './compoinents/TextForm';
+import React, {useState} from 'react';
 
 function App() {
+  // Create a function toggle mode listeral in App.js
+  //use useState
+  const [mode, setMode] = useState("light")//intinizalie for mode is false ("light")
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark")
+      document.body.style.backgroundColor = "#042743"
+    } else {
+      setMode("light")
+      document.body.style.backgroundColor = "white"
+    }
+  }
   return (
+    
     <>
-    <Navbar title="TextUtils" aboutText="About"/>
+    {/* <Navbar title="TextUtils" aboutText="About"/> */}
     {/* <Navbar /> */}
+    <Navbar title="TextUtils" mode = {mode} toggleMode = {toggleMode}/>
     <div className="container my-3">
-     <TextForm heading="Enter the text to analyze bellow"/> 
+     <TextForm heading="Enter the text to analyze bellow" mode={mode}/> 
+     {/* <About/> */}
     </div>
     
     </>
--- src/compoinents/TextForm.js
+++ src/compoinents/TextForm.js
@@ -17,27 +17,54 @@ export default function TextForm(props) {
   const handleLoClick = () => {
     let newText = text.toLowerCase()
     setText(newText)
+  } 
+// Credit: A
+  const handleCopy = () => {
+      navigator.clipboard.writeText(text)
+
   }
+
+  // Credit: Code raha
+  const handleExtraSpace = () => {
+    let newText = text.split(/ [ ] + /)
+    setText(newText.join(" "))
+  }
+
+  //Credit: shift
+  const [theme, setTheme] = useState("light")
+
+  const handleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark")
+    } else {
+      setTheme("light")
+    }
+  }
+
   const [text, setText] = useState('')
   // text ='new text here' //Wrong way to change the state
   //setText("new text") //Correct way to change the state
+  
   return (
     <>
-    <div className='container'>
+    <div className='container' data-bs-theme = {theme} style={{color: props.mode==="dark" ? "white" : "#042743"}}>
       <h1>{props.heading}</h1>
         <div className="mb-3">
-          <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+          <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==="dark" ?'gray':'white', color: props.mode==='dark' ? 'white' : '#042743'}} id="myBox" rows="8"></textarea>
           <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
           <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
           <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
+          <button className='btn btn-primary mx-1' onClick={handleCopy}>Copy Text</button>
+          <button className='btn btn-primary mx-1' onClick={handleExtraSpace}>Remove Extra Space</button>
+           <button className='btn btn-primary mx-1' onClick={handleTheme}>Toggle Theme</button>
         </div>
     </div>
-    <div className="container my-2">
+    <div className="container my-3" style={{color: props.mode === 'dark' ? 'white' : '#042743'}}>
       <h1>Your text summary</h1>
       <p>{text.split(" ").length} words, {text.length} characters</p>
       <p>{ 0.008 *(text.split(" ").length) } Minutes read</p>
       <h2>Preview</h2>
-      <p>{text}</p>
+      <p>{text.length >0 ? text : "Your enter something in the texbox above to preview it here" }</p>
     </div>
     </>
     
